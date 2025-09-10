884a9460d2a0a87c878fb91ae0d58fd566d75515
LEECH3 <leech3@oocl.com>
2025-09-10 11:24:14 +0800

    refactor: update getEmployees, getEmployeeById and createEmployee in the EmployeeController, EmployeeRepository and EmployeeService

---
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getEmployees(String gender, Integer page, Integer size) {
+        return employeeRepository.getEmployees(gender, page, size);
+    }
+
+    public Employee getEmployeeById(int id) {
+        Employee employee = employeeRepository.getEmployeeById(id);
+        if (employee == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found with id: " + id);
+        }
+        return employee;
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.createEmployee(employee);
+    }
+
+}
