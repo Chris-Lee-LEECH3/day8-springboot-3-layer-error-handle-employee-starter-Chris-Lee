85640f65ee09eea746b9de4d663c0e8705013fc8
LEECH3 <leech3@oocl.com>
2025-09-11 14:09:19 +0800

    refactor: remove used import

--- src/test/java/com/example/demo/EmployeeServiceTest.java
+++ src/test/java/com/example/demo/EmployeeServiceTest.java
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTest {
     @InjectMocks
-    private EmployeeService employeeService;;
+    private EmployeeService employeeService;
 
     @Mock
     private IEmployeeRepository employeeRepository;
